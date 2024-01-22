7da35b51c84573295bd04dbccf427f5a560c94a8
Yoon-Suji <jidl88@ewhain.net>
2024-01-22 14:59:59 +0900

    feat: JavaReflection 요구사항2 - test로 시작하는 메소드 실행 - test로 시작하는 메서드만 실행하도록 구현

--- src/test/java/next/reflection/Junit3Runner.java
+++ src/test/java/next/reflection/Junit3Runner.java
@@ -2,10 +2,26 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Junit3Runner {
     @Test
     public void runner() throws Exception {
         Class clazz = Junit3Test.class;
 
+        Arrays.stream(clazz.getDeclaredMethods()).filter((method -> {
+            return method.getName().startsWith("test");
+        })).forEach((method -> {
+            try {
+                method.invoke(clazz.newInstance());
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            }
+        }));
     }
 }
