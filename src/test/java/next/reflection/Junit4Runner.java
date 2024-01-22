b6b09c68fd5a3def7faad594a08825f3a3580cac
Yoon-Suji <jidl88@ewhain.net>
2024-01-22 15:16:24 +0900

    feat: JavaReflection 요구사항3 - @Test 애노테이션 메소드 실행 - getDeclaredMethods() 로 가져온 method의 isAnnotationPresent() 메서드를 이용해 @MyTest 어노테이션이 붙은 메서드만 실행하도록 구현

--- src/test/java/next/reflection/Junit4Runner.java
+++ src/test/java/next/reflection/Junit4Runner.java
@@ -2,11 +2,27 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Junit4Runner {
     @Test
     public void run() throws Exception {
         Class clazz = Junit4Test.class;
 
+        Arrays.stream(clazz.getDeclaredMethods()).filter((method -> {
+            return method.isAnnotationPresent(MyTest.class);
+        })).forEach((method -> {
+            try {
+                method.invoke(clazz.newInstance());
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            }
+        }));
     }
 }
 
